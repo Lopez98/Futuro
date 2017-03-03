d54f5bfab574302f395f045995bf1e3c4a186791
Estudiantes <Estudiantes@LABING50206.udistrital.edu.co>
2017-03-03 18:02:10 -0500

    version 1.0

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head lang="es">
+        <meta charset="UTF-8">
+        <title>Mi futuro</title>
+    </head>
+    <body>
+        <h1>Dentro de 15 años...</h1>
+        <?php
+        echo "Quiero tener <b>mi propio proyecto</b> enfocado al desarrollo de sofware"
+        . ",aplicativos móviles, videojuegos. ";
+        ?>
+    </body>
+</html>
